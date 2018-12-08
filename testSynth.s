b5570f7fdc5088b078faa7b982670c14b9987f47
lephoquebleu <35134735+lephoquebleu@users.noreply.github.com>
2018-12-08 15:24:56 +0100

    Add files via upload

---
+++ testSynth.s
@@ -0,0 +1,85 @@
+.text
+
+ori $v0, $0, 31
+li $a0, 60
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 60
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 60
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 62
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 64
+li $a1, 1000
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 62
+li $a1, 1000
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 60
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 64
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 62
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 62
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 60
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall
+
+ori $v0, $0, 31
+li $a0, 72
+li $a1, 500
+li $a2, 115
+li $a3, 63
+syscall

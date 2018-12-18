a195c9b2f84f0209182f542f33ffd598426e02a4
Harian-Elyoth <harian.elyoth@gmail.com>
2018-12-18 12:29:37 +0100

    Fonctionnalité de Folie et Essaie de Beat

---
+++ EssayeBeat.s
@@ -0,0 +1,32 @@
+	.text
+
+	li $a0, 60
+	li $a2, 8
+	li $a1, 400
+	li $a3, 60
+	li $v0, 33
+	syscall
+	li $a0, 60
+	li $a2, 9
+	li $a1, 400
+	li $a3, 60
+	li $v0, 33
+	syscall
+	li $a0, 60
+	li $a2, 10
+	li $a1, 400
+	li $a3, 60
+	li $v0, 33
+	syscall
+	li $a0, 60
+	li $a2, 11
+	li $a1, 400
+	li $a3, 60
+	li $v0, 33
+	syscall
+	li $a0, 60
+	li $a2, 12
+	li $a1, 400
+	li $a3, 60
+	li $v0, 33
+	syscall
--- EssayeReactionClavier.s
+++ EssayeReactionClavier.s
@@ -12,25 +12,38 @@ LA :	.word 69
 SIb :	.word 70
 SI :	.word 71
 DO1 :	.word 72
+msgB :	.asciiz "Bienvenu sur le super Synthé 2000 !!\n Les notes sont réparties entre la lettre q : Do et k : Do\n"
+msgI :	.asciiz "Choisi un instrument !\n0-7   Piano	\n64-71    Reed \n8-15    Chromatic Percussion\n72-79    Pipe \n16-23    Organ	\n80-87    Synth Lead \n24-31    Guitar	\n88-95    Synth Pad \n32-39    Bass	\n96-103    Synth Effects \n40-47    Strings	\n104-111    Ethnic \n48-55    Ensemble	\n112-119    Percussion \n56-63    Brass	\n120-127    Sound Effects\nAppuie sur BACKSPACE pour quitter\n"
+
 
 	.text
 	
-	
+affiche:li $v0, 4
+	la $a0, msgB
+	syscall
+message:la $a0, msgI
+	syscall
+	li $v0	5
+	syscall
+	add $s7, $v0, $0
+	li $s6, 32 #CODE TOUCHE ECHAP
+	beq $s7, $s6, FIN
 Choix :	li $v0, 12
 	syscall
 	li $t1, 113 	#q
 	li $t2, 122	#z
 	li $t3, 115	#s
-	li $t4, 251	#e
-	li $t5, 250	#d
-	li $t6, 252	#f
+	li $t4, 101	#e
+	li $t5, 100	#d
+	li $t6, 102	#f
 	li $t7, 116	#t
-	li $t8, 253	#g
+	li $t8, 103	#g
 	li $t9, 121	#y
-	li $s0, 254	#h
+	li $s0, 104	#h
 	li $s1, 117	#u
-	li $s2, 256	#j
-	li $s3, 257	#k
+	li $s2, 106	#j
+	li $s3, 107	#k
+	li $s4, 32 #BACKSPACE POUR QUITTER LE MODE JOUER ET RETOURNER DANS LE MODE CHOISIR UN INSTRUMENT
 	beq $v0, $t1, JoueDo
 	beq $v0, $t2, JoueReb
 	beq $v0, $t3, JoueRe
@@ -44,112 +57,113 @@ Choix :	li $v0, 12
 	beq $v0, $s1, JoueSib
 	beq $v0, $s2, JoueSi
 	beq $v0, $s3, JoueDo1
+	beq $v0, $s4, message
 	j Choix
 	
 JoueDo:	lw $a0, DO
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueReb:	lw $a0, REb
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 
 JoueRe:	lw $a0, RE
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueMib:	lw $a0, MIb
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueMi:	lw $a0, MI
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueFa:	lw $a0, FA
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueSolb:	lw $a0, SOLb
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueSol:	lw $a0, SOL
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueLab:	lw $a0, LAb
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueLa:	lw $a0, LA
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueSib:	lw $a0, SIb
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueSi:	lw $a0, SI
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
 	
 JoueDo1:	lw $a0, DO1
-	li $a2, 5
-	li $a1, 25
+	add $a2, $s7, $0
+	li $a1, 400
 	li $a3, 60
 	li $v0, 33
 	syscall
 	j Choix
-	
 
-	
+FIN :	li $v0, 10
+	syscall
 	

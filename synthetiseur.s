5a6f700f61dd75b43dd415a29e96014f922ded8f
Harian-Elyoth <harian.elyoth@gmail.com>
2018-12-20 10:58:52 +0100

    Ajout Synthétiseur

---
+++ synthetiseur.s
@@ -0,0 +1,6 @@
+	.data
+	
+TabN :	.word 60, 61, 62, 63 ,64, 65, 66, 67, 68, 69, 70, 71, 72
+TabT : 	.byte 'q', 'z', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h', 'u', 'j', 'k'
+Bienv :	.asciiz "Bienvenue sur le super Synthé 2000 !!\n Les notes sont réparties entre la lettre q : Do et k : Do\n"
+msgI :	.asciiz "Choisis un instrument !\n0-7   Piano	\n64-71    Reed \n8-15    Chromatic Percussion\n72-79    Pipe \n16-23    Organ	\n80-87    Synth Lead \n24-31    Guitar	\n88-95    Synth Pad \n32-39    Bass	\n96-103    Synth Effects \n40-47    Strings	\n104-111    Ethnic \n48-55    Ensemble	\n112-119    Percussion \n56-63    Brass	\n120-127    Sound Effects\nAppuie sur q pour quitter\n"

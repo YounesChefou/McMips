823165909e2d1dd76bb5afffd4cf6f87c5a1352d
Harian-Elyoth <harian.elyoth@gmail.com>
2018-12-20 12:49:09 +0100

    Ajout Base Projet

--- synthetiseur.s
+++ synthetiseur.s
@@ -5,25 +5,30 @@ TabN :	.word 60, 61, 62, 63 ,64, 65, 66, 67, 68, 69, 70, 71, 72
 TabT : 	.byte 'q', 'z', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h', 'u', 'j', 'k'
 TabJ :	.word 60, 60, 62, 60, 64, 65, 60
 msgB :	.asciiz "Bienvenue sur le super Synthé 2000 !!\n Les notes sont réparties entre la lettre q : Do et k : Do\n"
-msgI :	.ascii "Choisis un instrument !\n0-7   Piano	\n64-71    Reed \n"
-	.ascii "8-15    Chromatic Percussion\n72-79    Pipe \n16-23    Organ	\n"
-	.ascii "80-87    Synth Lead \n24-31    Guitar	\n88-95    Synth Pad \n32-39    Bass	\n"
-	.ascii "96-103    Synth Effects \n40-47    Strings	\n104-111    Ethnic \n48-55    Ensemble	\n"
-	.asciiz "112-119    Percussion \n56-63    Brass	\n120-127    Sound Effects\nAppuie sur q pour quitter\n"
+msgI :	.ascii "\nChoisis un instrument !\n0-7   Piano	\n"
+	.ascii "8-15    Chromatic Percussion\n16-23    Organ	\n24-31    Guitar\n32-39    Bass\n"
+	.ascii "40-47    Strings\n48-55    Ensemble \n56-63    Brass\n64-71    Reed\n72-79    Pipe \n80-87    Synth Lead \n88-95    Synth Pad \n"
+	.ascii "96-103    Synth Effects	\n104-111    Ethnic 	\n"
+	.asciiz "112-119    Percussion	\n120-127    Sound Effects\nAppuie sur 128 pour quitter\n"
 msgF :	.asciiz "Merci d'avoir joué !"
 
 	.text
 
-	la $t0, TabJ
+######### DEBUT ############
+
+Jingl :	la $t0, TabJ
 	addi $t3, $t3, 0
-	li $a1, 250
-Jingl :	beq $t3, 6, Bienv
+	li $a1, 150
+loop1 :	beq $t3, 6, Bienv
 	jal Joue
 	addi $t0, $t0, 4
 	addi $t3, $t3, 1
-	j Jingl
+	li $v0, 32
+	li $a0, 250
+	syscall
+	j loop1
 	
-	
+
 	
 Bienv :	li $v0, 4
 	la $a0, msgB
@@ -34,14 +39,19 @@ ChoixI :la $a0, msgI
 	li $v0	5
 	syscall
 	addi $a2, $v0, 0
-	#beq $v0, 'q', FIN #A REVOIR
+	beq $v0, 128, FIN #A REVOIR
 	li $a1, 400
+	
+########### MAIN ##################
+
 LireChar:li $v0, 12	#lire un charactere
 	syscall
 	beq $v0, 'm', ChoixI #Si il appuie sur m : retour sur le Menu Instrument
 	jal fJoueNote #Joue la note correspondante
 	j LireChar	#relis une note
 	
+########### FONCTIONS ##############	
+
 fJoueNote :	la $t0, TabN #$t0 contient le tableau de Note
 	la $t1, TabT	#$t1 contient le tableau de touche
 Cond :	lb $t2, 0($t1) #contient un caractere du tableau de caractere
@@ -50,11 +60,16 @@ Cond :	lb $t2, 0($t1) #contient un caractere du tableau de caractere
 	addi $t0, $t0, 4 #On passe a la note suivante
 	j Cond
 Joue :	lw $a0, 0($t0) #Parametre de la note
-	li $v0, 33 #Syscall pour jouer la note
+	li $v0, 31 #Syscall pour jouer la note
 	li $a1, 400 #duration
 	li $a3, 60
 	syscall
 	jr $ra
+	
+########### FIN ###################
+	
+FIN : 	li $v0, 10
+	syscall
 
 
 	
